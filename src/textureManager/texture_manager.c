7193d9422d8a229aa29cc815b8b2011d22eec649
Jakub Ducho <87933202+Ghost9887@users.noreply.github.com>
2025-08-26 17:42:01 +0200

    Delete src directory

--- src/textureManager/texture_manager.c
+++
@@ -1,7 +0,0 @@
-#include "texture_manager.h"
-
-void loadAllTextures(TextureManager *textureManager){
-  textureManager->playerTexture = LoadTexture("assets/player/player.png");
-  textureManager->basicEnemyTexture = LoadTexture("assets/enemy/basic_enemy.png");
-  textureManager->pistolTexture = LoadTexture("assets/weapons/pistol.png");
-}
